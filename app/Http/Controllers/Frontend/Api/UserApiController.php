8073d191733dc0d621cba35b58b404babd02688c
shahed141123 <khandkershahed23@gmail.com>
2025-08-19 14:41:32 +0600

    Attendance Log

--- app/Http/Controllers/Frontend/Api/UserApiController.php
+++ app/Http/Controllers/Frontend/Api/UserApiController.php
@@ -582,21 +582,20 @@ class UserApiController extends Controller
         $user = $request->user();
 
         $bookings = Booking::with([
-            'user:id,name,email',
-            'event:id,name,start_date,start_time,venue,end_date,end_time'
-        ])
+                'user:id,name,email',
+                'event:id,name,start_date,start_time,venue,end_date,end_time'
+            ])
             ->where('user_id', $user->id)
             ->get();
-        // Attach seat detailsto each booking
+        // Attach seat details to each booking
         $bookings->transform(function ($booking) {
             $eventSeats = json_decode($booking->event_seats, true);
             $seatIds = $eventSeats['seat_ids'] ?? [];
             // Fetch seat details
             $seats = EventSeat::whereIn('id', $seatIds)
-                ->get(['name', 'code', 'price']);
+                ->get(['name', 'code', 'price', 'row', 'column', 'status']);
 
             $booking->seats = $seats;
-
             return $booking;
         });
 
